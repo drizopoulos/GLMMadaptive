a8e4457299eb939fc20ef5ab030e9c2bf48ea9cd
Dimitris Rizopoulos <d.rizopoulos@erasmusmc.nl>
2020-10-25 22:28:03 +0100

    updates

--- R/mixed_model.R
+++ R/mixed_model.R
@@ -38,7 +38,9 @@ mixed_model <- function (fixed, random, data, family, weights = NULL,
         family <- Gamma.fam()
     }
     known_families <- c("binomial", "poisson", "negative binomial", "Gamma")
-    data <- orig_data <- as.data.frame(data) # in case 'data' is a tibble
+    if (inherits(data, "tbl_df") || inherits(data, "tbl"))
+        data <- as.data.frame(data) # in case 'data' is a tibble
+    orig_data <- data
     groups <- unique(c(all.vars(getID_Formula(random)), 
                        if (!is.null(zi_random)) all.vars(getID_Formula(zi_random))))
     data[groups] <- lapply(data[groups], function (x) if (!is.factor(x)) factor(x) else x)
